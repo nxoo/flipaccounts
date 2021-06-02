e66f1cc6caa2868260ed8df260c47c99e2f4d182
nxoo <brianwangombe11@gmail.com>
2021-06-02 18:14:30 +0300

    added border to navbar toggler

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -156,10 +156,6 @@ export default function Navbar() {
                 box-shadow: none;
               }
 
-              .navbar-toggler {
-                border: none;
-              }
-
               @media (min-width: 992px) {
                 #inbox-mobile {
                   display: none;
