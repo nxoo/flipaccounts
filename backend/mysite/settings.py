8a5a5dec5e9c3c9c099d24885172650d71690325
nxoo <brianwangombe11@gmail.com>
2021-06-03 18:58:53 +0300

    added allowed hosts to env

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -36,7 +36,8 @@ DEBUG = env('DEBUG')
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = [] if not any(ALLOWED_HOSTS) else ALLOWED_HOSTS
 
 # Application definition
 
