b3ab77435c67838fad850932aa9a6654da92497f
nxoo <brianwangombe11@gmail.com>
2021-07-01 23:42:50 +0300

    new alert

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -12,7 +12,7 @@ export default function Layout({children}) {
             <Navbar/>
             <div className='alert alert-warning alert-dismissible fade show' role="alert">
                 <div className="col-sm-6 mx-auto">
-                Beta! some features won't work
+                Beta! some features may not work
                 </div>
             </div>
             <div className="container layout">
